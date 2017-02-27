d36e6c1244c54cfad1a81b58aa974ce14d92a218
赵董宏 <zdongh2010@sina.com>
2017-02-27 11:39:45 +0800

    add views core

---
+++ src/pages/public/header.vue
@@ -0,0 +1,9 @@
+<template> 
+    <header class="header clearfix">
+        <div class="f-l">GER错误监控系统</div>
+        <div class="f-r">
+            <router-link to="/user/modpwd">修改密码</router-link>
+            <a href="/logout"> 退出</a>
+        </div>
+    </header>
+</template>
---
+++ src/pages/public/menu.vue
@@ -0,0 +1,16 @@
+<template> 
+    <div class="ger-menu">
+        <ul>
+            <li>
+                <a href="">添加列表</a>
+            </li>
+            <li>
+                <a href="">错误列表</a>
+            </li>
+        </ul>
+    </div>
+</template> 
+<script> 
+</script>
+<style>
+</style>
