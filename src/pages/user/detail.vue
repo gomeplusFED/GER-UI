6f350be68c2cbb0faa56dc2aa69a8777af8bd912
suman030266 <13718327740@163.com>
2017-03-01 14:21:07 +0800

    user.vue update

---
+++ src/pages/user/detail.vue
@@ -0,0 +1,25 @@
+<template> 
+    <div>
+        <div class="right_area userdetail">
+            <p class="content_title">用户详情</p>
+            <ul>
+                <li>
+                    <label for="username">用户名: </label><input type="text" id="username" class="username">
+                </li>
+                <li>
+                    <label for="password">密　码: </label><input type="password" id="password" class="password">
+                </li>
+                <li class="clearfix">
+                    <label class="fl" for="domains">域名组: </label>
+                    <textarea class="domains" id="domains"></textarea>
+                </li>
+            </ul>
+            <div class="submit">
+                <input type="button" class="create" value="创建">
+                <input type="button" class="back" value="返回">
+            </div>
+        </div>
+    </div>
+</template> 
+<script> 
+</script>
