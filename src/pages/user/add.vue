8eaf486ac0d4d535cbf6adb31d485d3737b4cb81
suman030266 <13718327740@163.com>
2017-03-02 15:21:39 +0800

    update vuefile

--- src/pages/user/add.vue
+++ src/pages/user/add.vue
@@ -4,9 +4,9 @@
         	<p class="content_title">添加用户</p>
             <label for="username">用户名: </label><input type="text" id="username" class="username">
             <div class="submit">
-                <a href="#" class="create">创建</a>
+                <a href="#" class="btns create">创建</a>
                 <!-- <input type="button" class="back" value="返回"> -->
-            	<router-link to="/index"  active-class="active" class="back">返回</router-link>
+            	<router-link to="/index"  active-class="active" class="btns back">返回</router-link>
             </div>
         </div>
     </div>
