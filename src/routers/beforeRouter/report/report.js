a6f182b71938fe4adef7cc954da0dabb15904d89
赵董宏 <zdongh2010@sina.com>
2017-03-13 22:56:47 +0800

    update routers

--- src/routers/beforeRouter/report/report.js
+++ src/routers/beforeRouter/report/report.js
@@ -3,6 +3,15 @@
  * @fileoverview routers index.js
  * @date 2017/03/1
  */
-export default (obj)=>{
-	console.log(obj);
+
+
+export default (Vue, obj)=>{
+	let pageNum = obj.query.page ? obj.query.page : 0;
+	Vue.http.get('/report/getAll', {
+		page: pageNum
+	}).then(function(response ){
+		console.log(response )
+	}, function(response){
+		console.log(JSON.stringify(response));
+	});
 };
