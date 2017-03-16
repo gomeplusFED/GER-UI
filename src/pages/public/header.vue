fca7b65cf37768ac8215a4e197059db72aee2928
赵董宏 <zdongh2010@sina.com>
2017-03-16 11:21:59 +0800

    add username in header.vue

--- src/pages/public/header.vue
+++ src/pages/public/header.vue
@@ -2,6 +2,8 @@
     <header class="header clearfix">
         <div class="f-l">GER错误监控系统</div>
         <div class="f-r">
+        	<span class="f-l" v-if="!isAdmin">{{userName}}</span>
+        	<router-link :to="{ name: 'edit', query: { uname: userName, type: 'edit' }}" active-class="active" v-if="isAdmin">{{userName}}</router-link>
             <router-link :to="{ name: 'modpwd', query: { uname: userName }}" active-class="active">修改密码</router-link>
             <a href="/logout"> 退出</a>
         </div>
@@ -15,7 +17,8 @@
 	export default {
 	    computed: {
 	        ...mapState({
-	            userName:state => store.state.initModule.userName
+	            userName:state => store.state.initModule.userName,
+	            isAdmin: state => store.state.initModule.isAdmin
 	        })
 	    }
 	   
