85c24a256ee688cf4509fb871ccbb7929af6dc7d
suman030266 <13718327740@163.com>
2017-03-30 19:00:59 +0800

    update list.js

--- src/store/modules/report/list.js
+++ src/store/modules/report/list.js
@@ -1,5 +1,6 @@
 
 import Vue from  'vue';
+import echarts from  'echarts';
 const state = {
     lists:{},
     buckets: {},
@@ -30,6 +31,12 @@ const mutations = {
     },
     'SEARCH_ECHAR': state => {
         console.log(state);
+
+        // let options = {};
+
+        console.log(echarts);
+        // echarts.init(document.getElementsByClass('report-charbox')[0]);
+        // myChart.setOption(options);
     },
     'SEARCH': state => {
         /*if( selectDay !== 1 || ){
